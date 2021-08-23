870d352fd945224170e113f9d398eb8241f455cb
NekoIceCream <nekoicecream@outlook.co.id>
2021-08-23 14:16:32 +0700

    Update

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Starting Build"
+echo "WARNING : YOU ONLY CAN BUILD IN ARCH BASED!"
+echo ""
+sleep 2
+
+sudo pacman -Syyyu
+sudo pacman -S archiso --noconfirm
+repo-add (calameres i forget it)
+# STILL IN PROGRESS
+
+
+# Copyright (c) 2021 NekoIceCream

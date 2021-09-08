0cf43a1436fb6b684cfedb7c98c4fba9d50672bf
NekoIceCream <nekoicecream@outlook.co.id>
2021-09-08 18:04:27 +0700

    Update Build Script

--- build_x86_64.sh
+++ build_x86_64.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 # Your Only Can Build On Arch Based System
 
+clear
+echo "==================================="
+echo "       IceLinux Build Script       "
+echo "==================================="
+echo "- Arch : x86_64"
+echo "- Building Date : $(date +%Y.%m.%d)"
+echo "- Script Version : v1.0"
+echo ""
+echo "<https://github.com/NekoIceTeam/IceLinux>"
+echo "  Copyright (c) 2021 NekoIceCream  "
+echo "==================================="
+echo ""
+echo "       Warning Before Building"
+echo "- Building Only Work On Arch Based System"
+echo "- It's Take 4GB+ For Download Packages"
+echo "- Need good Internet Connection"
+echo ""
+echo "Starting Building..."
+echo "Ctrl + For Cancel"
+sleep 4
+
 sudo pacman -Syyyu
 sudo pacman -S archiso --noconfirm
 mkarchiso -v -w ./work -o ./iso ./icelinux_x86_64

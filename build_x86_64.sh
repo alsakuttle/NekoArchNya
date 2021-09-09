dde1615df785cc5ff48659c9d1f192f01e3a2395
NekoIceCream <nekoicecream@outlook.co.id>
2021-09-09 14:48:01 +0700

    Update Directory

--- build_x86_64.sh
+++ build_x86_64.sh
@@ -24,6 +24,6 @@ sleep 10
 
 sudo pacman -Syyyu --noconfirm
 sudo pacman -S archiso --noconfirm
-mkarchiso -v -w icelinux_x86_64/work/ -o icelinux_x86_64/iso/ icelinux_x86_64/
+mkarchiso -v -g 8E02DA8285638FD2 -G nekoicecream@outlook.co.id -w icelinux_x86_64/work -o icelinux_x86_64/iso icelinux_x86_64/
 
 # Copyright (c) 2021 NekoIceCream

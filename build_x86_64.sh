cf9c0003f72c6721783d8a5b2057ce45c369f495
Kucing Nolep <nekoicecream@outlook.co.id>
2021-10-12 22:09:23 +0700

    Update build_x86_64.sh

--- build_x86_64.sh
+++ build_x86_64.sh
@@ -10,20 +10,12 @@ echo "       IceLinux Build Script       "
 echo "==================================="
 echo "- Arch : x86_64"
 echo "- Building Date : $(date +%Y.%m.%d)"
-echo "- Script Version : v1.0"
+echo "- Script Version : v2.0"
 echo ""
 echo "<https://github.com/NekoIceTeam/IceLinux>"
 echo "  Copyright (c) 2021 NekoIceCream  "
 echo "==================================="
-echo ""
-echo "       Warning Before Building"
-echo "- Building Only Work On Arch Based System"
-echo "- It's Take 4GB+ For Download Packages"
-echo "- Need good Internet Connection"
-echo ""
-echo "Starting Building..."
-echo "Ctrl + For Cancel"
-sleep 4
+
 
 if [ -d $OUT_DIR ]
 then
@@ -42,5 +34,4 @@ sudo pacman -Syyyu --noconfirm
 sudo pacman -S archiso --noconfirm
 mkarchiso -v -g 8E02DA8285638FD2 -G nekoicecream@outlook.co.id -w $BUILD_DIR -o $OUT_DIR icelinux_x86_64/
 #mkarchiso -v -G nekoicecream@outlook.co.id -w $BUILD_DIR -o $OUT_DIR ./icelinux_x86_64
-sleep 2
 # Copyright (c) 2021 NekoIceCream
